c630ba2c600f18f5e1442ea2aa861baf4398d345
mauricio.minella@bravi.com.br <mauricio.minella@bravi.com.br>
2016-01-22 12:12:25 -0200

    Sinon and Stubs done

--- test/sinonTests.js
+++ test/sinonTests.js
@@ -6,20 +6,49 @@ var chai = require('chai'),
 chai.should();
 
 describe('sinon tests', function(){
-  var student;
+  var student, schedule;
 
   beforeEach(function(){
     student = {
         dropClass: function(classId, cb){
           //do stuff
-          cb();
+          if(!!cb.dropClass){
+            cb.dropClass();
+          }else{
+            cb();
+          }
+        }
+    };
+
+    schedule = {
+        dropClass: function(){
+          console.log('class dropped');
         }
     };
   });
 
   describe('student.dropClass', function(){
     it('should call the callback', function(){
-        student.dropClass();
+        var spy = sinon.spy();
+        student.dropClass(1, spy);
+        spy.called.should.be.true;
+    });
+
+    it('should call the callback and log to the console', function(){
+        function onClassDropped(){
+          console.log('onClassDropped was called');
+        }
+
+        var spy = sinon.spy(onClassDropped);
+
+        student.dropClass(1, spy);
+        spy.called.should.be.true;
+    });
+
+    it('should call the callback even if it\'s a method of an object', function(){
+      sinon.spy(schedule, 'dropClass');
+      student.dropClass(1, schedule);
+      schedule.dropClass.called.should.be.true;
     });
   });
 

e24a33ebff9e6f4dc743ae810dab5ca14509cb03
Mauricio Minella <mauriciominella@gmail.com>
2016-01-23 19:13:05 -0200

    Testing error done. Next: Finding Edge Cases

--- test/courseTests.js
+++ test/courseTests.js
@@ -9,7 +9,13 @@ var Student = require('../Student')
   describe('Course', function(){
     var courseName = 'Introduction to Awesomeness',
         courseCode = 'AWE 101',
-        courseDescription = 'This course will make you awesome!';
+        courseDescription = 'This course will make you awesome!',
+        student;
+
+
+        beforeEach(function(){
+         student = Student.create('Mauricio Minella', 5);
+       });
 
         it('should save data correctly', function(){
           var course = Course.create(courseName, courseCode, courseDescription);
@@ -23,5 +29,26 @@ var Student = require('../Student')
 
           should.exist(course.times);
           course.times.should.eql([]);
+        });
+
+        describe('registerStudent', function(){
+          it('should add the student to the students array', function(){
+            var course = Course.create(courseName, courseCode, courseDescription);
+
+            course.registerStudent(student);
+
+            course.students.length.should.equal(1);
+            course.students[0].id.should.equal(student.id);
+          });
+        });
+
+        describe('unregisterStudent', function(){
+          it("should throw an error if we try to remove a student that isn't in the class", function(){
+            var course = Course.create(courseName, courseCode, courseDescription);
+
+            expect(function(){
+              course.unregisterStudent('asdf');
+            }).to.throw();
+          });
         });
   });
